6c2459495ad3540af4e2f38f37e62e94c178a6d5
soushi52 <soushi.b.0502@gmail.com>
2025-09-24 11:51:20 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,18 @@
+package code11_ex1;
+
+public abstract class TangibleAsset {
+	String name;
+	int price;
+	String color;
+	
+	public TangibleAsset(String name,int price,String color) {
+		this.name = name;
+		this.price = price;
+		this.color = color;
+	}
+	public String getName() {return this.name; }
+	public int getPrice() {return this.price; }
+	public String color() {return this.color;}
+	
+
+}
